9e3ee81b75d1f576f7ec28c3e6eef40963afe7fa
louglecompany <144390491+louglecompany@users.noreply.github.com>
2023-12-19 08:57:48 -0300

    Update script.js

--- flip-eleven/script.js
+++ flip-eleven/script.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // Adiciona um ouvinte de clique para o botão Início
     btnInicio.addEventListener('click', function() {
         // Redireciona para a página desejada no GitHub (substitua 'SEU-USUARIO' e 'SEU-REPOSITORIO' pelos seus dados)
-        window.location.href = '//index.html';
+        window.location.href = ' https://louglecompany.github.io/infinito/';
     });
 });
