1f0c21de2240eeb646162f48d28b4bc4a63c9ba9
louglecompany <144390491+louglecompany@users.noreply.github.com>
2023-12-19 08:22:17 -0300

    Delete flip-eleven directory

--- flip-eleven/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var btnInicio = document.getElementById('btnInicio');
-
-    // Adiciona um ouvinte de clique para o botão Início
-    btnInicio.addEventListener('click', function() {
-        // Redireciona para a página desejada no GitHub (substitua 'SEU-USUARIO' e 'SEU-REPOSITORIO' pelos seus dados)
-        window.location.href = '//index.html';
-    });
-});
